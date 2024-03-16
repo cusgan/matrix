9ebb2cbeef0ab57cdc1ab3b8a0c952af1f3069c2
Wens <141469454+cusgan@users.noreply.github.com>
2024-03-16 21:56:13 +0800

    added square determinant

--- matrixoperations.h
+++ matrixoperations.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 using namespace std;
 
 vector<vector<int>> createMatrix(string name) {
@@ -42,9 +43,25 @@ vector<vector<int>> subtract(vector<vector<int>> A, vector<vector<int>> B) {
     return res;
 }
 
+//integer
 vector<vector<int>> multiply(vector<vector<int>> A, vector<vector<int>> B) {
     int row = A.size(), col = B[0].size();
     vector<vector<int>> res(row, vector<int>(col));
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            res[i][j] = 0;  
+            for (int k = 0; k < col; k++) {
+                res[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+    return res;
+}
+
+//double
+vector<vector<double>> multiply(vector<vector<double>> A, vector<vector<double>> B) {
+    int row = A.size(), col = B[0].size();
+    vector<vector<double>> res(row, vector<double>(col));
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
             res[i][j] = 0;  
@@ -64,4 +81,30 @@ void displayMatrix(vector<vector<int>> mx) {
 		cout << endl;
 	}
 	cout << endl;
+}
+
+int determinantSquare(vector<vector<int>> mx) {
+    int sz = mx.size();
+    if (sz == 2) {
+        return mx[0][0] * mx[1][1] - mx[0][1] * mx[1][0];
+    }
+    
+    int det = 0;
+    for (int i = 0; i < sz; i++) {
+        vector<vector<int>> inner(sz-1, vector<int>(0));
+        for (int j = 1; j < sz; j++) { //rows
+            int k = 0;
+            while (inner[j-1].size() < sz-1) { //columns
+                if (k == i) {
+                    k++;
+                    continue;
+                }
+                inner[j-1].push_back(mx[j][k]);
+                k++;
+            }
+        }
+        det += mx[0][i] * pow(-1, i) * determinantSquare(inner);
+    }
+
+    return det;
 }
